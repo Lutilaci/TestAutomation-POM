8ccad7e00d7b4ab1913f8269e99b0b19bb8ca811
Krisz <placebo1314@gmail.com>
2022-08-17 09:40:19 +0200

    LogInLogout back (temporarily)

--- src/test/java/com/codecool/testautomation/test/LogOutTest.java
+++ src/test/java/com/codecool/testautomation/test/LogOutTest.java
@@ -1,5 +1,6 @@
 package com.codecool.testautomation.test;
 
+import com.codecool.testautomation.utility.LogInLogout;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
