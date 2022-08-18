4b6d7c395cbf68686cc36a1da395ed805162f389
Lutilaci <lutilaci@gmail.com>
2022-08-18 20:47:03 +0200

    refactor Utility

--- src/test/java/com/codecool/testautomation/utility/Utility.java
+++ src/test/java/com/codecool/testautomation/utility/Utility.java
@@ -3,28 +3,26 @@ package com.codecool.testautomation.utility;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
- import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 public class Utility {
-    public static WebDriver driver = Driver.getInstance().getDriver();
-    public static WebDriverWait wait = Driver.getWait();
-    public static final String baseUrl = "https://jira-auto.codecool.metastage.net/";
+    static WebDriver driver = DriverSingleton.getDriver();
 
+    public static final String baseUrl = "https://jira-auto.codecool.metastage.net/";
 
     public static void clickButton(WebElement webElement){webElement.click();
     }
 
     public static void waitForWebElementToBePresent(WebElement webElement){
-        wait.until(ExpectedConditions.visibilityOf(webElement));
+        DriverSingleton.getWait().until(ExpectedConditions.visibilityOf(webElement));
     }
 
     public static void waitForElementToSendText(WebElement webElement, String text){
-        wait.until(ExpectedConditions.elementToBeClickable(webElement)).sendKeys(text);
+        DriverSingleton.getWait().until(ExpectedConditions.elementToBeClickable(webElement)).sendKeys(text);
 
     }
     public static void waitForElementToClick(WebElement webElement){
-        wait.until(ExpectedConditions.elementToBeClickable(
+        DriverSingleton.getWait().until(ExpectedConditions.elementToBeClickable(
                 webElement
         )).click();
     }
@@ -50,6 +48,6 @@ public class Utility {
     }
 
     public static void close(){
-        driver.close();
+        DriverSingleton.getDriver().close();
     }
 }
